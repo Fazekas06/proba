5897719833fcdf6c31fb2cc8bda83db7dadd7249
Fazekas06 <fazekasmarkkrisztian@turr.hu>
2023-12-12 08:38:25 +0100

    abb

--- ize.py
+++ ize.py
@@ -1,6 +1,7 @@
 a=int(input("ad meg az a oldalt"))
 b=int(input("ad meg a b oldalt"))
 eredmeny=a+b
+eredmeny2=a*b
 x=input("kerület, vagy, területet kell kiszámolni?")
 if x==("terület"):
     print
